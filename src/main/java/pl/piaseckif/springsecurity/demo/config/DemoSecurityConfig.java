f8b28c19e1b6720990d321aef6a80e26f0dc2748
Franciszek Piasecki <frrenk.p@gmail.com>
2019-03-13 22:36:28 +0100

    added role restriction added custom 403 page hid links to role restricted pages from unauthorized accounts changed user authorization form in memory to database

--- src/main/java/pl/piaseckif/springsecurity/demo/config/DemoSecurityConfig.java
+++ src/main/java/pl/piaseckif/springsecurity/demo/config/DemoSecurityConfig.java
@@ -1,5 +1,6 @@
 package pl.piaseckif.springsecurity.demo.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -8,22 +9,25 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.User.UserBuilder;
 
+import javax.sql.DataSource;
+
 
 @Configuration
 @EnableWebSecurity
 public class DemoSecurityConfig extends WebSecurityConfigurerAdapter {
 
+    @Autowired
+    private DataSource securityDataSource;
+
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		
-		//add our users for in memory authentication
-		
-		UserBuilder users = User.withDefaultPasswordEncoder();
-		
-		auth.inMemoryAuthentication().withUser(users.username("john").password("test123").roles("EMPLOYEE"));
-		auth.inMemoryAuthentication().withUser(users.username("mary").password("test456").roles("EMPLOYEE", "MANAGER"));
-		auth.inMemoryAuthentication().withUser(users.username("susan").password("test789").roles("EMPLOYEE", "ADMIN"));
-	}
+
+        //use jdbc authentication
+	    auth.jdbcAuthentication().dataSource(securityDataSource);
+
+    }
+
+
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
